b4ac6535437a35f572cb69abf9d13b6d0f19a5a7
Evan Ische <SixTilesSquared@gmail.com>
2016-04-20 01:55:22 -0500

    Initial Commit

---
+++ UIColor+Categories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIColor+Categories
+//
+//  Created by Evan Ische on 4/20/16.
+//  Copyright © 2016 Evan William Ische. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIColor+Categories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIColor+Categories
+//
+//  Created by Evan Ische on 4/20/16.
+//  Copyright © 2016 Evan William Ische. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
